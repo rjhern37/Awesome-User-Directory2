f586411bef42f8476f8c0645924c8e42c21b4795
Ray H <rjhern37@neiu.edu>
2020-07-15 13:45:12 -0500

    setting up

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header";
+import EzTable from "./components/EzTable";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <EzTable />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
